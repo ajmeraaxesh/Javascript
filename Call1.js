7b002366f4c9c15b6294f8ddd21f2077812cd88d
Axesh R. Ajmera <ajmeraaxesh@gmail.com>
2011-09-11 13:50:22 -0700

    Adding some initial files

---
+++ Call1.js
@@ -0,0 +1,22 @@
+<!--
+	Demonstrates  use of 
+	
+-->
+<html>
+	<head>
+		<script type = "text/javascript">
+		//<![CDATA[
+		
+		function sayQuote(){
+			console.log("Quote: '"+this.quote+"'");
+		}
+		
+		//Creating different objects of author
+		var quote1 = {
+			"quote":
+		}
+		
+		//]]>
+		</script>
+	</head>
+</html>
